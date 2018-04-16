9073709b82fb9bf88c72a739720078164d840bb3
日高彩伽 <ayaka@hidakaayaka-no-MacBook-Air.local>
2018-04-16 23:08:21 +0900

    Create users list page

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -27,4 +27,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+
+  test "successful edit with frendly forwarding" do
+    get edit_user_path(@user)
+    assert_not session[:forwarding_url].nil?
+    log_in_as(@user)    
+    assert_redirected_to edit_user_url(@user)
+    name = "FooBar"
+    email = "foo@bar.com"
+    patch user_path(@user), params:{user:{name: name, email: email, password: "",password_confirmation: ""}}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    assert session[:forwarding_url].nil?
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
